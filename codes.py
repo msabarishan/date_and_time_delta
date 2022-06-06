335cb607bdd4d4480e1d99e8ed58656f6170ed0a
msabarishan <103972490+msabarishan@users.noreply.github.com>
2022-06-06 21:28:50 +0530

    Update codes.py

--- codes.py
+++ codes.py
@@ -1,1 +1,7 @@
-
+from datetime import date, timedelta
+# time delta is used to display past date.
+today =date.today()
+# to display date before 30 days
+lastmonth = today - timedelta(days=30)
+date_stamp = lastmonth.strftime("%d %B, %Y")
+print(date_stamp)
